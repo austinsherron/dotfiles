03c1b13f7379cfa6908f8e5ca18f1c6a481917a1
Austin Sherron <austin@pryvate.email>
2023-03-27 04:34:09 -0700

    [WIP] Finish Consolidating bashrc
    * Temp fix for nvmin init issues
    * Minor tmux + git conf updates

---
+++ system/shell/bash/machine-spc/austins01-sigma/script_hooks.sh
@@ -0,0 +1,8 @@
+<< comment
+This script function as a a place to keep machine specific config/functionality 
+related to using shared scripts.
+comment
+
+
+## vc-mount ####################################################################
+
