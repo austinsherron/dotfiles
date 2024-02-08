733b6df6df030d1c4d1acf10b8694d23b6128cef
Austin Sherron <austin@pryvate.email>
2024-02-08 09:55:42 -0800

    [Refactor] Reorganize Repo
    * Move actual dotfiles to new "chezmoi" directory
    * Update chezmoi.toml to reflect new source dir
    * Update install script to reflect new source dir
    * Move .chezmoidata files to new "manifest" directory
    * Sym-link manifest/* to .chezmoidata
    * Move .chezmoiscripts files to new "scripts/hooks" directory
    * Sym-link scripts/hooks/* to .chezmoiscripts
    * Update record versions hook to record individual tmux script versions
    * Update tmux installation script to check individual tmux script
      versions before deploying
    * Add "tree" to darwin manifest
    * Update README.md

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,25 +7,29 @@ set -Eeuo pipefail
 
 ## run os specific bootstrap script
 
+DOTFILES="$(dirname $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P ))"
+
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
-source "${SCRIPT_PATH}/bootstrap/${OS}/main.sh"
+source "${DOTFILES}/scripts/bootstrap/${OS}/main.sh"
 
 ## install chezmoi config
 
-SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 CONFIG_ROOT="${HOME}/.config"
-CHEZMOI_DIR="${CONFIG_ROOT}/chezmoi"
-CHEZMOI_TOML_SRC="${SCRIPT_PATH}/dot_config/chezmoi/chezmoi.toml"
-CHEZMOI_TOML_TGT="${CHEZMOI_DIR}/chezmoi.toml"
 
-if [[ ! -d "${CHEZMOI_DIR}" ]]; then
-    echo "[INFO] creating ${CHEZMOI_DIR}"
-    mkdir "${CHEZMOI_DIR}"
+CHEZMOI_SRC="${DOTFILES}/chezmoi"
+CHEZMOI_DST="${CONFIG_ROOT}/chezmoi"
+
+CHEZMOI_TOML_SRC="${CHEZMOI_SRC}/dot_config/chezmoi/chezmoi.toml"
+CHEZMOI_TOML_DST="${CHEZMOI_DST}/chezmoi.toml"
+
+if [[ ! -d "${CHEZMOI_DST}" ]]; then
+    echo "[INFO] creating ${CHEZMOI_DST}"
+    mkdir "${CHEZMOI_DST}"
 fi
 
-if [[ ! -f "${CHEZMOI_TOML_TGT}" ]]; then
-    echo "[INFO] copying ${CHEZMOI_TOML_SRC} to ${CHEZMOI_TOML_TGT}"
-    cp "${CHEZMOI_TOML_SRC}" "${CHEZMOI_TOML_TGT}"
+if [[ ! -f "${CHEZMOI_TOML_DST}" ]]; then
+    echo "[INFO] copying ${CHEZMOI_TOML_SRC} to ${CHEZMOI_TOML_DST}"
+    cp "${CHEZMOI_TOML_SRC}" "${CHEZMOI_TOML_DST}"
 fi
 
 ## apply chezmoi
