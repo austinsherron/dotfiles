ca34198143b9b58b732545a48b8aea59efc1d503
Austin Sherron <austin@pryvate.email>
2024-03-02 18:51:43 -0800

    [Enhance] Miscellaneous Enhancements
    * Add new utility aliases
    * Add function for displaying a command's output in a tmux popup
    * Update standard paths to use XDG conformant env vars
    * Remove quotes from XDG path env values
    * Add tmux key bindings for switching sessions
    * Remove "window-pane-changed" tmux hook
    * Update tmux layouts
    * Add nvim spell dir and hammerspoon paths to chezmoiignore
    * Move LOG_ROOT definition from bash_profile to shared_paths.sh
    * Add misc git aliases

--- chezmoi/dot_config/shell/bash/helpers/aliases.sh
+++ chezmoi/dot_config/shell/bash/helpers/aliases.sh
@@ -7,6 +7,7 @@ alias c="clear"
 alias db="dropbox"
 alias g="git"
 alias nv="nvim"
+alias spt="spotify_player"
 alias tk="task"
 alias tkui="taskwarrior-tui"
 alias tx="tmux"
@@ -32,9 +33,11 @@ alias dfd="dfdot"
 
 # deploy
 alias deploy-dot="chezmoi diff && chezmoi apply && rcload && txload"
+alias deploy-dot-force="chezmoi apply && rcload && txload"
 alias dpdot="deploy-dot"
-alias dpdot="deploy-dot"
+alias dpdotf="deploy-dot-force"
 alias dpd="dpdot"
+alias dpdf="dpdotf"
 
 ### edit
 
@@ -71,16 +74,6 @@ alias diffw="diff -w"
 alias pbc="pbcopy"
 alias pbp="pbpaste"
 
-### search
-
-alias brv="s -p searxng"
-alias ddg="s -p duckduckgo"
-alias goog="s -p google"
-alias qwnt="s -p qwant"
-alias sp="s -p startpage"
-alias srx="s -p searxng"
-alias sanon="s -p google -b \"chromium --incognito\""
-
 ### github
 
 ## prs
@@ -93,4 +86,29 @@ alias gh-pr-view="gh pr view ${1}"
 ## repos
 
 alias gh-repo-ls="gh repo ls --no-archived"
+
+### repls
+
+alias hs-pop="tmux-cmd-popup \"hs\""
+alias lua-pop="tmux-cmd-popup \"lua\""
+alias py-pop="tmux-cmd-popup \"python3\""
+
+### search
+
+alias brv="s -p searxng"
+alias ddg="s -p duckduckgo"
+alias goog="s -p google"
+alias qwnt="s -p qwant"
+alias sp="s -p startpage"
+alias srx="s -p searxng"
+alias sanon="s -p google -b \"chromium --incognito\""
+
+## alias functions #############################################################
+
+### informational
+
+function fz-history() { history | fzf-tmux -p; }
+function help() { tmux-cmd-popup "${1} --help"; }
+function man() { tmux-cmd-popup "man ${1}"; }
+function tldr() { tmux-cmd-popup "tldr ${1}" -h 70%; }
 
